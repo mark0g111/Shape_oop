9c7ed736df0fc3bf7de4c5d17772f94523f13c5d
mark0g111 <9154547225@mail.ru>
2022-12-17 15:27:12 +0300

    Add sides value check

--- triangle.py
+++ triangle.py
@@ -3,12 +3,16 @@ from math import sqrt
 
 
 class Triangle(Shape):
+    MIN_VALUE = 0
 
     def __init__(self, side_a, side_b, side_c):
         super().__init__()
-        self.side_a = side_a
-        self.side_b = side_b
-        self.side_c = side_c
+        if Triangle.validate(side_a) and Triangle.validate(side_b) and Triangle.validate(side_c):
+            self.side_a = side_a
+            self.side_b = side_b
+            self.side_c = side_c
+        else:
+            raise ValueError
 
     def perimeter(self):
         return self.side_a + self.side_b + self.side_c
@@ -16,6 +20,12 @@ class Triangle(Shape):
     def square(self):
         p = (self.side_a + self.side_b + self.side_c) / 2
         return sqrt(p * (p - self.side_a) * (p - self.side_b) * (p - self.side_c))
+
+    @classmethod
+    def validate(cls, value):
+        if value > cls.MIN_VALUE:
+            return True
+        return False
 
     def __str__(self):
         return f'Triangle with sides {self.side_a}, {self.side_b} and {self.side_c}'
