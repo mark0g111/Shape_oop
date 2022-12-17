fa310d3c71b2360aeb4071966e42f3707da1f66c
mark0g111 <9154547225@mail.ru>
2022-12-17 13:35:11 +0300

    Made attributes private, add properties

--- rectangle.py
+++ rectangle.py
@@ -5,20 +5,30 @@ class Rectangle(Shape):
 
     def __init__(self, side_a, side_b):
         super().__init__()
-        self.side_a = side_a
-        self.side_b = side_b
+        self.__side_a = side_a
+        self.__side_b = side_b
 
-    def get_side_a(self):
-        return self.side_a
+    @property
+    def side_a(self):
+        return self.__side_a
 
-    def get_side_b(self):
-        return self.side_b
+    @side_a.setter
+    def side_a(self, new_value):
+        self.__side_a = new_value
+
+    @property
+    def side_b(self):
+        return self.__side_b
+
+    @side_b.setter
+    def side_b(self, new_value):
+        self.__side_b = new_value
 
     def perimeter(self):
-        return 2 * (self.side_a + self.side_b)
+        return 2 * (self.__side_a + self.__side_b)
 
     def square(self):
-        return self.side_a * self.side_b
+        return self.__side_a * self.__side_b
 
     def __str__(self):
-        return f'Rectangle with sides {self.side_a} and {self.side_b}'
+        return f'Rectangle with sides {self.__side_a} and {self.__side_b}'
