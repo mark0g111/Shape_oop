a74f28468b0174c4a423ed5a8a816c2659b699a3
mark0g111 <9154547225@mail.ru>
2022-12-20 16:30:27 +0300

    Did shape class as an abstract class

--- shape.py
+++ shape.py
@@ -1,4 +1,16 @@
-class Shape:
+from abc import ABC
+from abc import abstractmethod
+
+
+class Shape(ABC):
 
     def __init__(self):
+        super().__init__()
+
+    @abstractmethod
+    def perimeter(self):
+        pass
+
+    @abstractmethod
+    def square(self):
         pass
