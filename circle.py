eb946c6f68f5bcb3665ddbe61cfe1b9442f4c881
mark0g111 <9154547225@mail.ru>
2022-12-20 12:20:22 +0300

    Add eq and repr

--- circle.py
+++ circle.py
@@ -12,5 +12,13 @@ class Circle:
     def square(self):
         return round(pi * (self.radius ** 2), 2)
 
+    def __eq__(self, other):
+        if isinstance(other, Circle):
+            return other.radius == self.radius
+        return False
+
+    def __repr__(self):
+        return f'Circle({self.radius})'
+
     def __str__(self):
         return f'Circle with radius {self.radius}'
